d67fa98617c1899a39bc99e8e9c66bd219109f1a
Martez Reed <martez.reed@gmail.com>
2019-02-21 22:50:02 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,7 @@
 variable "image" {}
-variable "access_key" {}
-variable "secret_key" {}
-variable "region" {
-  default = "us-east-1"
-}
 
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region     = "${var.region}"
+  region = "us-east-1"
 }
 
 terraform {
