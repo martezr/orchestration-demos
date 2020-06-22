049f4f38a3dfa45faa13e6018d79325e999e2115
martezr <martez.reed@greenreedtech.com>
2020-06-21 23:43:16 -0500

    Fix syntax issue

--- terraform/chaosstack/main.tf
+++ terraform/chaosstack/main.tf
@@ -78,7 +78,7 @@ resource "aws_lb_target_group" "chaos_tg" {
 
 resource "aws_autoscaling_attachment" "asg_attachment_bar" {
   autoscaling_group_name = aws_autoscaling_group.chaos_asg.id
-  alb_target_group_arn   = aws_alb_target_group.chaos_tg.arn
+  alb_target_group_arn   = aws_lb_target_group.chaos_tg.arn
 }
 
 resource "aws_autoscaling_group" "chaos_asg" {
